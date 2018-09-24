9ea61264dc0989510b8bcbe45222ce5e38380946
bch-sitecore <43455979+bch-sitecore@users.noreply.github.com>
2018-09-24 00:37:03 +0000

    Added random password generator helper

---
+++ src/Unattended.SIF/Private/NewRandomPassword.ps1
@@ -0,0 +1,32 @@
+<#
+  .SYNOPSIS
+    Generates a new random password.
+  .PARAMETER Length
+    The length of the new password.
+  .PARAMETER SecureString
+    Return the password as a secure string.
+#>
+Function NewRandomPassword {
+  Param(
+    [Parameter(Position = 0)]
+    [ValidateRange(1, 255)]
+    [int]$Length = 8
+    ,
+    [Parameter()]
+    [Alias("SecureString")]
+    [switch]$AsSecureString
+  )
+  Begin {
+    $numbers = 48..57
+    $lcLetters = 97..122
+    $ucLetter = 65..90
+  }
+  Process {
+    $password = ([char[]]($numbers + $lcLetters + $ucLetter) | Sort-Object { Get-Random })[0..($Length-1)] -join ""
+    If ($AsSecureString) {
+      ConvertTo-SecureString $password -AsPlainText -Force
+    } Else {
+      $password
+    }
+  }
+}
